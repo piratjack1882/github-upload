3896f8c08e21f7587e721a81648f0c8b86acf7bd
Jack Sparrow <piratjack1882@gmail.com>
2020-06-16 00:02:10 +0300

    7-th commit: add method sub(int a, int b) in Calculate.java

--- src/by/home/console/hello/Calculator.java
+++ src/by/home/console/hello/Calculator.java
@@ -2,8 +2,13 @@ package by.home.console.hello;
 
 public class Calculator {
 
-	public int sum(int a, int b){
+	public int sum(int a, int b) {
 		int result = a + b;
+		return result;
+	}
+
+	public int sub(int a, int b) {
+		int result = a - b;
 		return result;
 	}
 }
